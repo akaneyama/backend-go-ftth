b043f4d71a4edd8d562ffa6b399bb46349d294ad
akaneyama <daffapurwanto234@gmail.com>
2025-10-30 16:20:11 +0700

    add package

---
+++ ftth-be/models/packages.go
@@ -0,0 +1,14 @@
+package models
+
+import "time"
+
+type packages struct {
+	PackageID    int       `gorm:"type:int;primaryKey;autoIncrement" json:"package_id"`
+	PackageName  string    `gorm:"type:varchar(200)" json:"package_name"`
+	PackageLimit string    `gorm:"type:varchar(100)" json:"package_limit"` // 10/10 (upload / download)
+	PackagePrice int       `gorm:"type:int" json:"package_price"`
+	PackageDesc  string    `gorm:"type:varchar(255)" json:"package_desc"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt    time.Time `json:"updated_at"`
+	IsDeleted    int       `gorm:"default:0" json:"is_deleted"`
+}
